518f779ecbee121fcb8cf0241ce18a2aed638aa1
Matteo Croce <rootkit85@yahoo.it>
2010-07-05 23:19:29 +0200

    rewrite gui

--- gui/MainFrame.java
+++ gui/MainFrame.java
@@ -25,10 +25,10 @@ public class MainFrame extends JFrame implements ActionListener
 	private File contextFile, expressionFile;
 	private EditorPanel editorPanel;
 
-	public static void main (String[] args)
+	/*public static void main (String[] args)
 	{
 		new MainFrame().setVisible(true);
-	}
+	}*/
 
 	public MainFrame ()
 	{
