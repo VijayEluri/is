c97650d10952e7749edf51f4ae18276e2f96ab93
Matteo Croce <rootkit85@yahoo.it>
2010-07-06 19:30:37 +0200

    add the mani gui

--- Main.java
+++ Main.java
@@ -5,10 +5,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -16,7 +20,7 @@ import javax.swing.JTextArea;
 public class Main extends JFrame implements ActionListener {
 	private JPanel result = new JPanel();
 	private JComboBox choice = new JComboBox(new String[]{"Evaluation", "Infix Print", "Postfix print", "Graph"});
-	private JTextArea expr = new JTextArea("0 == 0", 3, 20);
+	private JTextArea expr = new JTextArea("", 3, 20);
 
 	public static void main(String args[]) {
 		new Main();
@@ -28,26 +32,36 @@ public class Main extends JFrame implements ActionListener {
 		add(expr);
 		JButton go = new JButton("GO"); 
 		go.addActionListener(this);
+		add(choice);
 		add(go);
 		add(result);
 		pack();
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg) {
-		PropertiesContext context = new PropertiesContext ();
-		Expression expression;
 		try {
-			expression = parse(expr.getText());
+			result.removeAll();
+			PropertiesContext context = new PropertiesContext ();
+			JLabel jlabel = null;
+			Expression expression = parse(expr.getText());
 			switch(choice.getSelectedIndex()) {
-			case 0:	EvaluatingVisitor visitor = new EvaluatingVisitor (context);
-				visitor.valueOf (expression);
+			case 0:	jlabel = new JLabel("" + new EvaluatingVisitor (context).valueOf (expression));
 				break;
-			case 1:
-			case 2:
-			case 3:
+			case 1:	StringWriter iwriter = new StringWriter();
+				new InfixPrinterVisitor(iwriter).print (expression);
+				jlabel = new JLabel(iwriter.toString ());
+				break;
+			case 2:	StringWriter pwriter = new StringWriter();
+				new PostfixPrinterVisitor (pwriter).print (expression);
+				jlabel = new JLabel(pwriter.toString ());
+				break;
+			case 3:	jlabel = new JLabel(new ImageIcon (new PaintVisitor ().createImage (expression)));
 			}
+			result.add(jlabel);
+			pack();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -55,11 +69,8 @@ public class Main extends JFrame implements ActionListener {
 
 	private Expression parse(String expression) throws IOException
 	{
-		StringReader reader = new StringReader(expression);
-		StreamTokenizerAdapter sta = new StreamTokenizerAdapter(reader);
 		ExpressionBuilder builder = new ExpressionBuilder();
-		Parser parser = new Parser(sta, builder);
-		parser.parse();
+		new Parser(new StreamTokenizerAdapter(new StringReader(expression)), builder).parse();
 		return builder.getResult();
 	}
 }
