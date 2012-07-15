6a20493763394213f41239dd89494f56158e3ed8
Matteo Croce <rootkit85@yahoo.it>
2012-07-15 23:01:16 +0200

    add icons evaluate expression while typing

--- ecv/Main.java
+++ ecv/Main.java
@@ -1,19 +1,21 @@
 package ecv;
 
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
+import javax.swing.Box;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import ecv.operator.Expression;
 import ecv.visitor.EvaluatingVisitor;
@@ -22,10 +24,18 @@ import ecv.visitor.PaintVisitor;
 import ecv.visitor.PostfixPrinterVisitor;
 
 @SuppressWarnings("serial")
-public class Main extends JFrame implements ActionListener {
-	private JPanel result = new JPanel();
-	private JComboBox<String> choice = new JComboBox<String>(new String[]{"Evaluation", "Infix Print", "Postfix print", "Graph"});
+public class Main extends JFrame implements DocumentListener {
+	private JLabel err = new JLabel();
+	private ImageIcon ok = new ImageIcon("ok.png");
+	private ImageIcon fail = new ImageIcon("fail.png");
+	private JLabel valid = new JLabel(ok);
 	private JTextArea expr = new JTextArea("6 + 2^2 > 25 / 5 && 24 / 100 / 5^5 != 3 + 10 - 6", 5, 30);
+
+	// visitors
+	private JLabel evaluating = new JLabel();
+	private JLabel infix = new JLabel();
+	private JLabel postfix = new JLabel();
+	private JLabel graph = new JLabel();
 
 	public static void main(String args[]) {
 		new Main();
@@ -33,43 +43,86 @@ public class Main extends JFrame implements ActionListener {
 
 	private Main() {
 		super("ECV");
-		setLayout(new FlowLayout());
-		add(expr);
-		JButton go = new JButton("GO"); 
-		go.addActionListener(this);
-		add(choice);
-		add(go);
-		add(result);
+
+		GridBagConstraints c = new GridBagConstraints();
+
+		setLayout(new GridBagLayout());
+		expr.getDocument().addDocumentListener(this);
+
+		add(expr, c);
+
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		add(valid, c);
+
+		add(err, c);
+
+		c.gridwidth = 1;
+		add(new JLabel("Evaluating visitor:"), c);
+		c.gridwidth = GridBagConstraints.REMAINDER;
+
+		add(Box.createRigidArea(new Dimension(100, 5)), c);
+		add(evaluating, c);
+
+		c.gridwidth = 1;
+		add(new JLabel("Infix visitor:"), c);
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		add(infix, c);
+
+		c.gridwidth = 1;
+		add(new JLabel("Postfiv visitor:"), c);
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		add(postfix, c);
+
+		c.gridwidth = 1;
+		add(new JLabel("Graph visitor:"), c);
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		add(graph, c);
+
 		pack();
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent arg) {
-		try {
-			result.removeAll();
+	private void parse() {
+		if(expr.getText().length() == 0) {
+			valid.setIcon(ok);
+			err.setText(null);
+
+			evaluating.setText(null);
+			infix.setText(null);
+			postfix.setText(null);
+			graph.setIcon(null);			
+		} else try {
 			PropertiesContext context = new PropertiesContext ();
-			JLabel jlabel = null;
 			Expression expression = parse(expr.getText());
-			switch(choice.getSelectedIndex()) {
-			case 0:	jlabel = new JLabel("" + new EvaluatingVisitor (context).valueOf (expression));
-				break;
-			case 1:	StringWriter iwriter = new StringWriter();
-				new InfixPrinterVisitor(iwriter).print (expression);
-				jlabel = new JLabel(iwriter.toString ());
-				break;
-			case 2:	StringWriter pwriter = new StringWriter();
-				new PostfixPrinterVisitor (pwriter).print (expression);
-				jlabel = new JLabel(pwriter.toString ());
-				break;
-			case 3:	jlabel = new JLabel(new ImageIcon (new PaintVisitor ().createImage (expression)));
-			}
-			result.add(jlabel);
-			pack();
+
+			evaluating.setText("" + new EvaluatingVisitor(context).valueOf(expression));
+
+			StringWriter iwriter = new StringWriter();
+			new InfixPrinterVisitor(iwriter).print(expression);
+			infix.setText(iwriter.toString());
+
+			StringWriter pwriter = new StringWriter();
+			new PostfixPrinterVisitor(pwriter).print(expression);
+			postfix.setText("" + pwriter.toString());
+
+			graph.setIcon(new ImageIcon(new PaintVisitor().createImage(expression)));
+
+			valid.setIcon(ok);
+			err.setText(null);
+		} catch (InvalidExpressionError iee) {
+			valid.setIcon(fail);
+			err.setText(iee.getMessage());
+
+			evaluating.setText(null);
+			infix.setText(null);
+			postfix.setText(null);
+			graph.setIcon(null);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		pack();
 	}
 
 	private Expression parse(String expression) throws IOException
@@ -78,4 +131,17 @@ public class Main extends JFrame implements ActionListener {
 		new Parser(new StreamTokenizerAdapter(new StringReader(expression)), builder).parse();
 		return builder.getResult();
 	}
+
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		parse();
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		parse();
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) { }
 }
