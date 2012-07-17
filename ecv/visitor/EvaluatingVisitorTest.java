4d3220e0bfe489680d71e4f2e1b37bf942167b61
Matteo Croce <rootkit85@yahoo.it>
2012-07-17 15:26:55 +0200

    aggiunta relazione e test

---
+++ ecv/visitor/EvaluatingVisitorTest.java
@@ -0,0 +1,40 @@
+package ecv.visitor;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import ecv.InvalidExpressionError;
+import ecv.Parser;
+import ecv.PropertiesContext;
+import ecv.StreamTokenizerAdapter;
+import ecv.operator.Expression;
+import ecv.operator.ExpressionBuilder;
+
+
+public class EvaluatingVisitorTest {
+
+	private boolean evaluate(String expr) throws IOException {
+		ExpressionBuilder builder = new ExpressionBuilder();
+		new Parser(new StreamTokenizerAdapter(new StringReader(expr)), builder).parse();
+		Expression expression = builder.getResult();
+		return new EvaluatingVisitor(new PropertiesContext ()).valueOf(expression);		
+	}
+
+	@Test
+	public void testTrue() throws IOException {
+		Assert.assertTrue(evaluate("6 + 2^2 > 25 / 5 && 24 / [100 / 25] != 3 + 10 - 6"));
+	}
+
+	@Test
+	public void testFalse() throws IOException {
+		Assert.assertFalse(evaluate("10 == 4"));
+	}
+
+	@Test(expected=InvalidExpressionError.class)
+	public void testInvalid() throws IOException {
+		evaluate("10 == (");
+	}
+}
